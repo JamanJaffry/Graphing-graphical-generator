da299fef05510f0390a3509c6a4f834ab8a41191
Antonio <antonioallis@hotmail.co.uk>
2018-04-28 17:33:09 +0100

    new calc_qradatic_points() function added

--- graphics_graph.py
+++ graphics_graph.py
@@ -10,4 +10,17 @@ def calc_linear_points(start, stop, interval, m, c):
 			break
 	return points
 
+def calc_quadratic_points(start, stop, interval, a, b, c):
+	# This function returns a list of points from start to stop with a gap of interval for the equation y = ax^2 + bx + c
+	points = []
+	x = float(start)
+	while True:
+		y = a*(x**2) + b*x + c
+		points.append(y)
+		x += interval
+		if x > stop:
+			break
+	return points
+
 print(calc_linear_points(5, 10, 0.5, 2, 3))
+print(calc_quadratic_points(-7, 20, 2, 2, -4, 8))
